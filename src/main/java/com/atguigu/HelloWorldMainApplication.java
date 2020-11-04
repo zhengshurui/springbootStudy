67d85216fb4a575a3fdd5724b1ba27ebb437aa98
zhengshurui <1055759760@qq.com>
2020-11-04 22:27:45 +0800

    首次提交

--- src/main/java/com/atguigu/HelloWorldMainApplication.java
+++ src/main/java/com/atguigu/HelloWorldMainApplication.java
@@ -1,11 +1,14 @@
 package com.atguigu;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @springBootAppliction来标注一个主程序类,说明这是一个Spring Boot项目
  */
 @SpringBootApplication
 public class HelloWorldMainApplication {
     public static void main(String[] args) {
-git add
+        SpringApplication.run(HelloWorldMainApplication.class,args);
     }
 }
