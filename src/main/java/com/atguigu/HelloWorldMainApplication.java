f507823be95450815b0f5709c8e31f3c9160afec
zhengshurui <1055759760@qq.com>
2020-11-04 22:21:19 +0800

    首次提交

---
+++ src/main/java/com/atguigu/HelloWorldMainApplication.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @springBootAppliction来标注一个主程序类,说明这是一个Spring Boot项目
+ */
+@SpringBootApplication
+public class HelloWorldMainApplication {
+    public static void main(String[] args) {
+git add
+    }
+}
